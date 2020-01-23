130d511bf33f028a648c6d61e337b1e11a92d2c8
Schrese <53787633+Schrese@users.noreply.github.com>
2020-01-22 20:02:27 -0500

    finding the steps works

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -2,7 +2,8 @@ const db = require('../data/db-config.js');
 
 module.exports = {
     find,
-    findById
+    findById,
+    findSteps
 
 }
 
@@ -12,4 +13,16 @@ function find() {
 
 function findById(id) {
     return db('schemes').where({id}).first()
+}
+
+function findSteps(id) {
+    /* 
+        select scheme_name, step_number, instructions 
+        from steps as s
+        join schemes as sc on s.schemes_id = sc.id
+    */
+    return db.from('schemes')
+        .join('steps as s', 's.scheme_id', 'schemes.id')
+        .where('schemes.id', id)
+        .select('schemes.scheme_name', 's.step_number', 's.instructions')
 }
